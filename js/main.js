e837254ad5e08872ee0854da2e8fded466c412a5
Mery Ugoski <meryugoski@gmail.com>
2019-06-26 22:06:23 -0300

    terminei

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
-	var botao  = $('button');
-	var navbar = $('.navbar-brand');
-	var corNav = $('nav');
 
+	let botao = $('button');
+	let navbar = $('.navbar-brand');
+	let corNav = $('nav');
+    let bio = $('#bio');
+	let inputNome = $('#nome');
+	let inputBotao = $('#input-button');
 	botao.click(function(){
 		navbar.html(`
 		<img src="${dados.avatar_url}" class="avatar d-inline-block align-top">
 		${dados.name}
 		`);
-		navbar.addClass('d-flex align-items-center');
+		navbar.addClass('d-flex align-items-center')
 		corNav.toggleClass('trocar-cor bg-danger');
+       
+        bio.html(`${dados.bio}`);
+	});
+
+	inputBotao.click(function(){
+		let nome = inputNome.val();	
+		navbar.html(`
+			<img src="${dados.avatar_url}" class="avatar d-inline-block align-top">
+			${nome}
+		`);
 	});
 });
